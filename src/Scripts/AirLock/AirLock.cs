f67e7e1855905044816076c6b8afab38059f8c2b
Manuel Mayer <manumayerqc@gmail.com>
2022-05-04 20:51:28 -0400

    improved airlock script

--- src/Scripts/AirLock/AirLock.cs
+++ src/Scripts/AirLock/AirLock.cs
@@ -6,12 +6,10 @@ namespace IngameScript.Scripts.AirLock
 {
     public class Program : MyGridProgram
     {
-        private const string DOOR1 = "Station - Door01";
-        private const string DOOR2 = "Station - Door02";
         private const int ALLOWED_OPEN_SECOND = 5;
-        private readonly IMyDoor _door1;
-        private readonly IMyDoor _door2;
+        private readonly string[,] _doorGroups = { { "Station - Door01", "Station - Door02" } };
         private readonly Dictionary<long, DateTime> _doorOpenedCount = new Dictionary<long, DateTime>();
+        private readonly List<AirLockDoors> _doors = new List<AirLockDoors>();
 
         public Program()
         {
@@ -19,17 +17,26 @@ namespace IngameScript.Scripts.AirLock
 
             try
             {
-                _door1 = GridTerminalSystem.GetBlockWithName(DOOR1) as IMyDoor;
-                _door2 = GridTerminalSystem.GetBlockWithName(DOOR2) as IMyDoor;
+                for (var x = 0; x < _doorGroups.GetLength(0); x += 1)
+                {
+                    var door1 = GridTerminalSystem.GetBlockWithName(_doorGroups[x, 0]) as IMyDoor;
+                    var door2 = GridTerminalSystem.GetBlockWithName(_doorGroups[x, 1]) as IMyDoor;
 
-                if (_door1 == null)
-                    throw new Exception($"{DOOR1} block name does noes exists");
+                    if (door1 == null)
+                        throw new Exception($"{_doorGroups[x, 0]} block name does noes exists");
 
-                if (_door2 == null)
-                    throw new Exception($"{DOOR2} block name does noes exists");
+                    if (door2 == null)
+                        throw new Exception($"{_doorGroups[x, 1]} block name does noes exists");
 
-                _door1.ApplyAction("OnOff_Off");
-                _door2.ApplyAction("OnOff_Off");
+                    door1.ApplyAction("OnOff_Off");
+                    door2.ApplyAction("OnOff_Off");
+
+                    _doors.Add(new AirLockDoors
+                    {
+                        Door1 = door1,
+                        Door2 = door2
+                    });
+                }
             }
             catch (Exception ex)
             {
@@ -41,16 +48,19 @@ namespace IngameScript.Scripts.AirLock
 
         public void Main(string argument, UpdateType updateSource)
         {
-            _door1.ApplyAction(_door2.Status != DoorStatus.Closed
-                ? "OnOff_Off"
-                : "OnOff_On");
+            _doors.ForEach(doors =>
+            {
+                doors.Door1.ApplyAction(doors.Door2.Status != DoorStatus.Closed
+                    ? "OnOff_Off"
+                    : "OnOff_On");
 
-            _door2.ApplyAction(_door1.Status != DoorStatus.Closed
-                ? "OnOff_Off"
-                : "OnOff_On");
+                doors.Door2.ApplyAction(doors.Door1.Status != DoorStatus.Closed
+                    ? "OnOff_Off"
+                    : "OnOff_On");
 
-            UpdateDoorStatus(_door1);
-            UpdateDoorStatus(_door2);
+                UpdateDoorStatus(doors.Door1);
+                UpdateDoorStatus(doors.Door2);
+            });
         }
 
         private void UpdateDoorStatus(IMyDoor door)
@@ -67,6 +77,13 @@ namespace IngameScript.Scripts.AirLock
                 if (DateTime.Now.Subtract(_doorOpenedCount[door.EntityId]).TotalSeconds > ALLOWED_OPEN_SECOND)
                     door.CloseDoor();
             }
+        }
+
+        private class AirLockDoors
+        {
+            public IMyDoor Door1 { get; set; }
+
+            public IMyDoor Door2 { get; set; }
         }
     }
 }
