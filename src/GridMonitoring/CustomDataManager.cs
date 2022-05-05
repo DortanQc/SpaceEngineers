4f9e47eb81dc2a8c7c7e4462e79a736da6173dff
Manuel Mayer <manumayerqc@gmail.com>
2022-05-04 20:51:29 -0400

    wip of a GridMonitoring Script

---
+++ src/GridMonitoring/CustomDataManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IngameScript
+{
+    public class CustomDataManager
+    {
+        private readonly Dictionary<string, string> _settings;
+
+        public CustomDataManager()
+        {
+            _settings = new Dictionary<string, string>();
+        }
+
+        public CustomDataManager(string customData)
+        {
+            _settings = customData.Split(
+                    new[] { "\r\n", "\r", "\n" },
+                    StringSplitOptions.None)
+                .ToDictionary(
+                    s => s.Split('=')[0].Trim(),
+                    s =>
+                    {
+                        var setting = s.Split('=');
+
+                        return setting.Length > 1
+                            ? setting[1].Trim()
+                            : null;
+                    });
+        }
+
+        public string GetStringProperty(string key)
+        {
+            string value;
+
+            return _settings.TryGetValue(key, out value)
+                ? value
+                : null;
+        }
+
+        public void AddValue(string key, string value)
+        {
+            _settings.Add(key, value);
+        }
+    }
+}
---
+++ src/GridMonitoring/MonitoringData.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using VRage;
+using VRage.Game.ModAPI.Ingame;
+
+namespace IngameScript
+{
+    public class MonitoringData
+    {
+        private readonly Dictionary<string, int> _components;
+        private readonly CustomDataManager _dataManager = new CustomDataManager();
+
+        public MonitoringData()
+        {
+            MaxVolume = MyFixedPoint.Zero;
+            CurrentVolume = MyFixedPoint.Zero;
+            CurrentMass = MyFixedPoint.Zero;
+            CurrentOutput = 0f;
+            MaxOutput = 0f;
+            _components = new Dictionary<string, int>();
+        }
+
+        public MyFixedPoint MaxVolume
+        {
+            get
+            {
+                var value = _dataManager.GetStringProperty(nameof(MaxVolume));
+
+                return MyFixedPoint.DeserializeStringSafe(value);
+            }
+            set { _dataManager.AddValue(nameof(MaxVolume), value.SerializeString()); }
+        }
+
+        public MyFixedPoint CurrentVolume
+        {
+            get
+            {
+                var value = _dataManager.GetStringProperty(nameof(CurrentVolume));
+
+                return MyFixedPoint.DeserializeStringSafe(value);
+            }
+            set { _dataManager.AddValue(nameof(CurrentVolume), value.SerializeString()); }
+        }
+
+        public MyFixedPoint CurrentMass
+        {
+            get
+            {
+                var value = _dataManager.GetStringProperty(nameof(CurrentMass));
+
+                return MyFixedPoint.DeserializeStringSafe(value);
+            }
+            set { _dataManager.AddValue(nameof(CurrentMass), value.SerializeString()); }
+        }
+
+        public float CurrentOutput
+        {
+            get
+            {
+                var value = _dataManager.GetStringProperty(nameof(CurrentOutput));
+
+                return float.Parse(value);
+            }
+            set { _dataManager.AddValue(nameof(CurrentOutput), value.ToString(CultureInfo.InvariantCulture)); }
+        }
+
+        public float MaxOutput
+        {
+            get
+            {
+                var value = _dataManager.GetStringProperty(nameof(MaxOutput));
+
+                return float.Parse(value);
+            }
+            set { _dataManager.AddValue(nameof(MaxOutput), value.ToString(CultureInfo.InvariantCulture)); }
+        }
+
+        public void AddToInventory(MyInventoryItem item)
+        {
+            if (item.Type.GetItemInfo().IsComponent)
+                if (_components.ContainsKey(item.Type.SubtypeId))
+                    _components[item.Type.SubtypeId] += item.Amount.ToIntSafe();
+                else
+                    _components.Add(item.Type.SubtypeId, item.Amount.ToIntSafe());
+        }
+
+        public void Save() { }
+    }
+}
